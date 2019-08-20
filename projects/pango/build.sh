067d41517f516cbe4c474b31041f10c80bcbc0e8
Autofuzz team <security-tps@google.com>
2019-08-20 20:04:22 +0200

    Almost there

--- projects/pango/build.sh
+++ projects/pango/build.sh
@@ -1,6 +1,42 @@
 #!/bin/bash -eu
 
-meson _build
-ninja -C _build
+# configure script needs leak checking disabled to not fail
+export ASAN_OPTIONS=detect_leaks=0
 
-$CC $CFLAGS -c -std=c99 -I. -I./c/include c/parse_markup_fuzzer.c -o $OUT/parse_markup_fuzzer
+# Build a recent version of harbfuzz
+cd harfbuzz
+./autogen.sh 
+./configure --with-freetype=yes --with-gobject=yes
+make
+make install
+cd -
+
+cd pango
+build=$WORK/build
+
+# cleanup
+rm -rf $build
+mkdir -p $build
+
+meson $build \
+  -Db_lundef=false  \
+  -Dgtk_doc=false \
+  -Dinstall-tests=false \
+  #-Dintrospection=false  \
+
+ninja -C $build 
+#ninja -C $build install
+
+export GLIB_CFLAGS="$(pkg-config --static --cflags glib-2.0)"
+export HB_CFLAGS="$(pkg-config --static --cflags harfbuzz)"
+export CAIRO_CFLAGS="$(pkg-config --static --cflags cairo)"
+export GLIB_LIBS="$(pkg-config --static --libs glib-2.0)"
+export HB_LIBS="$(pkg-config --static --libs harfbuzz)"
+export CAIRO_LIBS="$(pkg-config --static --libs cairo)"
+$CC $CFLAGS \
+  -I. -I$build/pango/ -I$build -I./c/include ${GLIB_CFLAGS} ${HB_CFLAGS} ${CAIRO_CFLAGS}\
+  ${LIB_FUZZING_ENGINE} \
+  ${GLIB_LIBS} ${HB_LIBS} ${CAIRO_LIBS}\
+  ../parse_markup_fuzzer.c -o $OUT/parse_markup_fuzzer
+chmod +x $OUT/parse_markup_fuzzer
+$OUT/parse_markup_fuzzer
