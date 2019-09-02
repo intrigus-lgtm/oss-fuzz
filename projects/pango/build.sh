8399b6e805b4364c13687ee9f8a0bc6a74575d6b
Autofuzz team <security-tps@google.com>
2019-09-02 16:42:58 +0200

    It's now building!

--- projects/pango/build.sh
+++ projects/pango/build.sh
@@ -18,25 +18,31 @@ build=$WORK/build
 rm -rf $build
 mkdir -p $build
 
-meson $build \
+sed -i "/'-Werror=implicit-fallthrough',/d" meson.build
+
+CFLAGS="${CFLAGS} -Wno-implicit-fallthrough" meson $build \
   -Db_lundef=false  \
   -Dgtk_doc=false \
   -Dinstall-tests=false \
   #-Dintrospection=false  \
 
+
 ninja -C $build 
-#ninja -C $build install
+ninja -C $build install
 
 export GLIB_CFLAGS="$(pkg-config --static --cflags glib-2.0)"
 export HB_CFLAGS="$(pkg-config --static --cflags harfbuzz)"
 export CAIRO_CFLAGS="$(pkg-config --static --cflags cairo)"
+export PANGO_CFLAGS="$(pkg-config --static --cflags pango)"
 export GLIB_LIBS="$(pkg-config --static --libs glib-2.0)"
 export HB_LIBS="$(pkg-config --static --libs harfbuzz)"
 export CAIRO_LIBS="$(pkg-config --static --libs cairo)"
+export PANGO_LIBS="$(pkg-config --static --libs pango)"
+
 $CC $CFLAGS \
-  -I. -I$build/pango/ -I$build -I./c/include ${GLIB_CFLAGS} ${HB_CFLAGS} ${CAIRO_CFLAGS}\
+  -I. -I$build/pango/ -I$build -I./c/include ${GLIB_CFLAGS} ${HB_CFLAGS} ${CAIRO_CFLAGS} ${PANGO_CFLAGS}\
   ${LIB_FUZZING_ENGINE} \
-  ${GLIB_LIBS} ${HB_LIBS} ${CAIRO_LIBS}\
+  ${GLIB_LIBS} ${HB_LIBS} ${CAIRO_LIBS} ${PANGO_LIBS}\
   ../parse_markup_fuzzer.c -o $OUT/parse_markup_fuzzer
 chmod +x $OUT/parse_markup_fuzzer
 $OUT/parse_markup_fuzzer
