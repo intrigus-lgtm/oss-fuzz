00b20880b4233d58578cf6bbdeeedb6ba174a830
Google-Autofuzz <security-tps@google.com>
2020-02-19 16:03:17 -0500

    modified the fuzzer

--- projects/libimobiledevice/read_buffer_fuzzer.c
+++ projects/libimobiledevice/read_buffer_fuzzer.c
@@ -7,17 +7,7 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   const char* filename = fuzzer_get_tmpfile(data, size);
-  char* buffer = NULL;
-  uint64_t length;
-
-  if (!filename) return 0;
-
-  buffer_read_from_filename(filename, &buffer, &length);
-
-  if (!buffer) {
-    return 0;
-  }
-
-  free(buffer);
+  plist_t file_info = NULL;
+  plist_read_from_filename(&file_info, filename);
   return 0;
 }
